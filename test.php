fead848b3785df7c878c537289fea8ed48973a36
vyross <endhokgajah@gmail.com>
2021-10-26 14:04:00 +0700

    first-commit

---
+++ test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata</title>
+</head>
+<body>
+<table>
+	<td>
+		<tr>
+			Nama : Adi
+		</tr>
+	</td>
+	<td>
+		<tr>
+			Alamat : Gresik
+	</td>
+	<td>
+		<tr>
+			Usia : 25
+		</tr>
+	</td>
+</table>
+</body>
+</html>
